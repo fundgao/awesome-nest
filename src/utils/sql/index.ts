c8d7953321690a3e5356d39516d054255d4b0f14
fund <fundgao@163.com>
2024-03-05 21:00:38 +0800

    feat: nestjs 启动！

--- src/utils/sql/index.ts
+++ src/utils/sql/index.ts
@@ -17,3 +17,23 @@ export const whereOver = (sqlname: string, value: any) => {
 export const whereNotEmpty = (sqlname: string, value: any) => {
   return value ? `${sqlname} != '' and` : '';
 };
+
+//值在数组
+export const whereInArray = (sqlname: string, value: any) => {
+  if (value) {
+    const arr = value.split(',');
+
+    return `${sqlname} in (${arr.join()}) and`;
+  }
+  return '';
+};
+
+//值不在数组
+export const whereNotInArray = (sqlname: string, value: any) => {
+  if (value) {
+    const arr = value.split(',');
+
+    return `${sqlname} not in (${arr.join()}) and`;
+  }
+  return '';
+};
