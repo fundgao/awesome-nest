2f729aa88321de743f31718ac929ce9974912aac
fundgao <fundgao@gmail.com>
2024-04-04 14:56:58 +0800

    feat: nestjs 启动!

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,18 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
 export const getDateToString = (date: any) => {
   if (typeof date === 'string') {
     return date.replaceAll('/', '-');
   }
   return date;
 };
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext): any =>
+    context.switchToHttp().getRequest().user,
+);
+
+export const CurrentUserAgent = createParamDecorator(
+  (data: unknown, context: ExecutionContext): any =>
+    context.switchToHttp().getRequest().headers['user-agent'] || '',
+);
--- src/utils/sql/index.ts
+++ src/utils/sql/index.ts
@@ -8,17 +8,29 @@ export const whereLike = (sqlname: string, value: any) => {
   return value ? `${sqlname} like '%${value}%' and` : '';
 };
 
-//值大于
+// 值大于
 export const whereOver = (sqlname: string, value: any) => {
-  return value ? `${sqlname} >= '${value}' and` : '';
+  return value ? `${sqlname} > '${value}' and` : '';
 };
 
-//值大于
+// 值小于
+export const whereLess = (sqlname: string, value: any) => {
+  return value ? `${sqlname} < '${value}' and` : '';
+};
+
+// 两者之间
+export const whereBetween = (sqlname: string, value1: any, value2: any) => {
+  return value1 && value2
+    ? `${sqlname} between '${value1}' and '${value2}' and`
+    : '';
+};
+
+// 值不为空
 export const whereNotEmpty = (sqlname: string, value: any) => {
   return value ? `${sqlname} != '' and` : '';
 };
 
-//值在数组
+// 值在数组
 export const whereInArray = (sqlname: string, value: any) => {
   if (value) {
     const arr = value.split(',');
@@ -28,7 +40,7 @@ export const whereInArray = (sqlname: string, value: any) => {
   return '';
 };
 
-//值不在数组
+// 值不在数组
 export const whereNotInArray = (sqlname: string, value: any) => {
   if (value) {
     const arr = value.split(',');
